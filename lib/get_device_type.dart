c45df0e81f94207e8c7779b8bd5ce3e0f8d5272e
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2021-05-11 04:40:46 +0200

    Update get_device_type.dart

--- lib/get_device_type.dart
+++ lib/get_device_type.dart
@@ -3,13 +3,10 @@ part of responsive_widget;
 DeviceType getDeviceType(MediaQueryData mediaQueryData) {
   Orientation orientation = mediaQueryData.orientation;
   double width = 0;
-  switch (orientation) {
-    case Orientation.landscape:
-      width = mediaQueryData.size.height;
-      break;
-    case Orientation.portrait:
-      width = mediaQueryData.size.width;
-      break;
+  if(orientation==Orientation.landscape){
+    width = mediaQueryData.size.height;
+  }else{
+   width = mediaQueryData.size.width;
   }
   if (width >= 900) {
     return DeviceType.Desktop;
