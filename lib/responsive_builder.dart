f4b1f60c6caef3f441b4ae22ce8f907e4235edd7
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2021-06-01 19:29:51 +0200

    Update responsive_builder.dart

--- lib/responsive_builder.dart
+++ lib/responsive_builder.dart
@@ -5,10 +5,10 @@ part 'device_info.dart';
 part 'get_device_type.dart';
 part 'responsive_layout.dart';
 part 'device_type_enum.dart';
-class ResponsiveWidget extends StatelessWidget {
+class ResponsiveBuilder extends StatelessWidget {
   final Widget Function(BuildContext context, DeviceInfo deviceInfo)? builder;
   final double? width, height;
-  const ResponsiveWidget({Key? key, this.builder, this.width, this.height})
+  const ResponsiveBuilder({Key? key, this.builder, this.width, this.height})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
