1bd5c60844432d82f85d11d127d852bcd88b071b
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2022-04-15 06:38:21 +0200

    /

--- lib/get_device_type.dart
+++ lib/get_device_type.dart
@@ -7,15 +7,15 @@ DeviceType getDeviceType(MediaQueryData mediaQueryData) {
   if (orientation == Orientation.landscape) {
     if (Platform.isAndroid || Platform.isIOS || Platform.isFuchsia) {
       width = mediaQueryData.size.height;
-    } else {
-      width = mediaQueryData.size.width;
     }
+  } else {
+    width = mediaQueryData.size.width;
   }
   if (width >= 900) {
     return DeviceType.Desktop;
-  } else if (width >= 600 && width < 900) {
+  } else if (width > 600) {
     return DeviceType.Tablet;
-  } else if (width > 200 && width < 600) {
+  } else if (width > 200) {
     return DeviceType.Mobile;
   } else
     return DeviceType.Watch;
