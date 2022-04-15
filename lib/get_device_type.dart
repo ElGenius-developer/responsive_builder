c0dde98d72de142accb97993cc24aef82dc47342
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2022-04-15 06:19:09 +0200

    update width conditions

--- lib/get_device_type.dart
+++ lib/get_device_type.dart
@@ -12,15 +12,14 @@ DeviceType getDeviceType(MediaQueryData mediaQueryData) {
     width = mediaQueryData.size.width;
   }
 
-  if (width >= 950) {
+  if (width >= 900) {
     return DeviceType.Desktop;
   }
-  if (width > 600) {
+  if (width > 600 && width < 900) {
     return DeviceType.Tablet;
   }
-  if (width > 200) {
+  if (width > 200 && width < 600) {
     return DeviceType.Mobile;
-  }
-
-  return DeviceType.Watch;
+  } else
+    return DeviceType.Watch;
 }
