39149a2004294b4e20f04b4a9f41399e7ed279a2
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2022-04-16 04:15:59 +0200

    update get_device_platrform method

--- lib/get_device_platform.dart
+++ lib/get_device_platform.dart
@@ -1,25 +1,25 @@
 part of 'responsive_builder.dart';
 
-DevicePlatform getDevicePlatForm(BuildContext context) {
-  switch (Theme.of(context).platform) {
-    case TargetPlatform.android:
-      return DevicePlatform.Android;
-    case TargetPlatform.fuchsia:
-      return DevicePlatform.Fuchsia;
-    case TargetPlatform.iOS:
-      return DevicePlatform.IOS;
-    case TargetPlatform.linux:
-      return DevicePlatform.Linux;
-    case TargetPlatform.macOS:
-      return DevicePlatform.MacOS;
-    case TargetPlatform.windows:
-      return DevicePlatform.Windows;
-    default:
-      {
-        if (kIsWeb)
-          return DevicePlatform.Web;
-        else
-          return DevicePlatform.Embedded;
-      }
-  }
-}
+// DevicePlatform getDevicePlatForm(BuildContext context) {
+//   switch (Theme.of(context).platform) {
+//     case TargetPlatform.android:
+//       return DevicePlatform.Android;
+//     case TargetPlatform.fuchsia:
+//       return DevicePlatform.Fuchsia;
+//     case TargetPlatform.iOS:
+//       return DevicePlatform.IOS;
+//     case TargetPlatform.linux:
+//       return DevicePlatform.Linux;
+//     case TargetPlatform.macOS:
+//       return DevicePlatform.MacOS;
+//     case TargetPlatform.windows:
+//       return DevicePlatform.Windows;
+//     default:
+//       {
+//         if (kIsWeb)
+//           return DevicePlatform.Web;
+//         else
+//           return DevicePlatform.Embedded;
+//       }
+//   }
+// }
